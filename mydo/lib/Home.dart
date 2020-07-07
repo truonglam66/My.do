fe5d97fff94200e823c39e869a2572ba0192d1f5
Lam Truong <truongg166@gmail.com>
2020-07-07 09:32:48 +0700

    add Home

--- mydo/lib/Home.dart
+++ mydo/lib/Home.dart
@@ -1,47 +1,115 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'MyListGrid.dart' as mylistgridview;
-import 'TagsGrid.dart' as tagsgridview;
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mydo/Screens/DR_screen.dart';
+import 'package:mydo/Screens/meditation_screen.dart';
+import 'package:mydo/Screens/yoga_screen.dart';
+import 'package:mydo/Widgets/search_bar.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
+import 'Screens/session_screen.dart';
+import 'Widgets/bottom_nav_bar.dart';
+import 'Widgets/category_card.dart';
 
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  TabController controller;
-
-  @override
-  void initState() {
-    controller = new TabController(length: 2, vsync: this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Grid View"),
-          bottom: new TabBar(
-            controller: controller,
-            tabs: [
-              new Tab(text: ("Danh sách của bạn"),),
-              new Tab(text: ("Thẻ của bạn"),),
-            ],
-          ),
-        ),
-        body:new TabBarView(
-          controller: controller,
-          children:<Widget> [
-            new mylistgridview.MyListGrid(),
-            new tagsgridview.TagsGrid(),
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+        bottomNavigationBar: BottomNavBar(),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: size.height * .45,
+              decoration: BoxDecoration(
+                  color: Color(0xFFF5CEB8),
+                  image: DecorationImage(
+                      image:
+                          AssetImage("assets/image/undraw_pilates_gpdb.png"))),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        height: 52,
+                        width: 52,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF2BEA1),
+                          shape: BoxShape.circle,
+                        ),
+                        child: SvgPicture.asset("assets/icon/menu.svg"),
+                      ),
+                    ),
+                    Text(
+                      "Good morning \nLam Truong",
+                      style: Theme.of(context)
+                          .textTheme
+                          .display1
+                          .copyWith(fontWeight: FontWeight.w900),
+                    ),
+                    SearchBar(),
+                    Expanded(
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        childAspectRatio: .85,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        children: <Widget>[
+                          CategoryCard(
+                            title: "Diet Recommendation",
+                            svgSrc: "assets/icon/Hamburger.svg",
+                            press: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return DRScreen();
+                              }));
+                            },
+                          ),
+                          CategoryCard(
+                            title: "Kegel Execises",
+                            svgSrc: "assets/icon/Excrecises.svg",
+                            press: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return SessionScreen();
+                              }));
+                            },
+                          ),
+                          CategoryCard(
+                            title: "Meditation",
+                            svgSrc: "assets/icon/Meditation.svg",
+                            press: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return MeditationScreen();
+                              }));
+                            },
+                          ),
+                          CategoryCard(
+                            title: "Yoga",
+                            svgSrc: "assets/icon/yoga.svg",
+                            press: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return YogaScreen();
+                              }));
+                            },
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
-        )
-    );
+        ));
   }
 }
+
+
