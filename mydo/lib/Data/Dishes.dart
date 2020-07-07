b69b5eff10de7336892b5576a9ee7ea6fa3edb63
Lam Truong <truongg166@gmail.com>
2020-07-07 09:33:43 +0700

    add Data

---
+++ mydo/lib/Data/Dishes.dart
@@ -0,0 +1,26 @@
+List<Map> dishes = [
+  {
+    "image": "assets/image/image012.jpg",
+    "title": "Dish 01",
+    "time": "25 min",
+    "calories": "426 kCal"
+  },
+  {
+    "image": "assets/image/image013.jpg",
+    "title": "Dish 02",
+    "time": "10 min",
+    "calories": "200 kCal"
+  },
+  {
+    "image": "assets/image/image014.jpeg",
+    "title": "Dish 03",
+    "time": "40 min",
+    "calories": "634 kCal"
+  },
+  {
+    "image": "assets/image/image015.jpg",
+    "title": "Dish 04",
+    "time": "60 min",
+    "calories": "1.238 kCal"
+  },
+];
